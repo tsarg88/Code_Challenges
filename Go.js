83d487a2ff1a9ccb521f2afca9579d6d70300bbc
umbur <you@example.com>
2020-06-07 11:58:42 -0600

    completes go

---
+++ Go.js
@@ -0,0 +1,18 @@
+// Create a function that takes a number (from 1 - 60) and returns a corresponding string of hyphens.
+// Examples:
+// Go(1) ➞ "-"
+// Go(5) ➞ "-----"
+// Go(3) ➞ "---"
+// Notes:
+// You will be provided integers ranging from 1 to 60.
+// Don't forget to return your result as a string.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+function Go(num) {
+  let hyphens = "-";
+  return hyphens.repeat(num);
+}
+console.log(Go(1));
+console.log(Go(5));
+console.log(Go(3));
--- js_draft.js
+++ js_draft.js
@@ -1,9 +1,90 @@
-function stackBoxes(n) {
-  return n > 0 ? Math.pow(n, n) : 0;
-  //   if (n == 0) {
-  //     return 0;
-  //   } else {
-  //     return Math.pow(n, n);
-  //   }
-}
-console.log(stackBoxes(0));
+// function stackBoxes(n) {
+//   return n > 0 ? Math.pow(n, n) : 0;
+//   //   if (n == 0) {
+//   //     return 0;
+//   //   } else {
+//   //     return Math.pow(n, n);
+//   //   }
+// }
+// console.log(stackBoxes(0));
+
+// function flipBool(b) {
+//   return b == true ? 0 : 1;
+// }
+// console.log(flipBool(false));
+
+// function isPlural(word) {
+//   console.log(word.endsWith("s"));
+// }
+// console.log(isPlural("changes"));
+
+// function footballPoints(wins, draws, losses) {
+// 	return wins * 3 + draws * 1
+// }
+
+// function isOdd(num) {
+//   if (num % 2 != 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(isOdd(-5));
+
+// function validStrNumber(n) {
+//   if (isNaN(n)) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+// console.log(validStrNumber("3..3"));
+
+// function nSidedShape(n) {
+//   let obj = {
+//     1: "circle",
+//     2: "semi-circle",
+//     3: "triangle",
+//     4: "square",
+//     5: "pentagon",
+//     6: "hexagon",
+//     7: "heptagon",
+//     8: "octagon",
+//     9: "nonagon",
+//     10: "decagon",
+//   };
+//   return obj[n];
+// }
+// console.log(nSidedShape(2));
+
+// function longBurp(num) {
+//   let word = "r";
+//   return "Bu" + word.repeat(num) + "p";
+// }
+// console.log(longBurp(3));
+
+// function spaceMeOut(str) {
+//   return str.split("").join(" ");
+// }
+// console.log(spaceMeOut("space"));
+
+// function countWords(str) {
+//   return str.split(" ").length;
+// }
+// console.log(countWords("This is a test"));
+
+// function both(n1, n2) {
+//   if (n1 < 0 && n2 < 0) {
+//     return true;
+//   } else if (n1 > 0 && n2 > 0) {
+//     return true;
+//   } else if (n1 == 0 && n2 == 0) {
+//     return true;
+//   }
+// }
+// console.log(both(6, 2));
+
+// function findSmallestNum(arr) {
+//   return Math.min(...arr);
+// }
+// console.log(findSmallestNum([34, 15, 88, 2]));
