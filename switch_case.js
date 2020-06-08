7498f4b3db9a0b0bba7e50050d2e79d4b02598ba
umbur <you@example.com>
2020-06-08 17:15:59 -0600

    completes switch_case

---
+++ switch_case.js
@@ -0,0 +1,33 @@
+// Write a function, toEmoticon(), that takes in a string and returns the corresponding emoticon as a string. Use a switch/case, and cover these cases:
+// 'shrug' should return '|_{"}_|'
+// 'smiley face' should return ':)'
+// 'frowny face' should return':('
+// 'winky face' should return ';)'
+// 'heart' should return '<3'
+// any other input should return '|_(* ~ *)_|'
+
+// Write your function here:
+const toEmoticon = (str) => {
+  switch (str) {
+    case "shrug":
+      return '|_{"}_|';
+      break;
+    case "smiley face":
+      return ":)";
+      break;
+    case "frowny face":
+      return ":(";
+      break;
+    case "winky face":
+      return ";)";
+      break;
+    case "heart":
+      return "<3";
+      break;
+    default:
+      return "|_(* ~ *)_|";
+      break;
+  }
+};
+console.log(toEmoticon("whatever"));
+// Should print  '|_(* ~ *)_|'
