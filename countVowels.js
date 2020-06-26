5aac189b48aa1c189fd7852320356de0679e167a
umbur <you@example.com>
2020-06-26 10:47:15 +0400

    adds 2nd solution to countVowels

--- countVowels.js
+++ countVowels.js
@@ -7,8 +7,19 @@
 // a, e, i, o, u are considered vowels (not y).
 // All test cases are one word and only contain letters.
 
+// function countVowels(str) {
+//   return str.match(/[aeiou]/g).length;
+// }
+// console.log(countVowels("Celebration"));
+// console.log(countVowels("Palm"));
+
+// Method 2
+// const countVowels = (str) => {
+//   return str.split("").filter((x) => "aeiouAEIOU".includes(x).length);
+// };
+// console.log(countVowels("aabc"));
+
 function countVowels(str) {
-  return str.match(/[aeiou]/g).length;
+  return str.split("").filter((x) => "aeiouAEIOU".includes(x)).length;
 }
-console.log(countVowels("Celebration"));
-console.log(countVowels("Palm"));
+console.log(countVowels("aabc"));
