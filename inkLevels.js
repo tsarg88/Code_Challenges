74179fda452c246f4a8af81baade8ce5cf510316
umbur <work1415@gmail.com>
2020-07-10 15:59:32 +0400

    completes inkLevel

---
+++ inkLevels.js
@@ -0,0 +1,32 @@
+// Given an object of how many more pages each ink color can print, output the maximum number of pages the printer can print
+// before any of the colors run out.
+
+// Examples:
+// inkLevels({
+//     "cyan": 23,
+//     "magenta": 12,
+//     "yellow": 10
+//   }) ➞ 10
+//   inkLevels({
+//     "cyan": 432,
+//     "magenta": 543,
+//     "yellow": 777
+//   }) ➞ 432
+//   inkLevels({
+//     "cyan": 700,
+//     "magenta": 700,
+//     "yellow": 0
+//   }) ➞ 0
+
+function inkLevels(inks) {
+  let total = Object.values(inks);
+  return Math.min(...total);
+  //   console.log(...total);
+}
+console.log(
+  inkLevels({
+    cyan: 23,
+    magenta: 12,
+    yellow: 10,
+  })
+);
