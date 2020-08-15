a8fdbe30eee0a4a604b003689884677abd66b09b
umbur <work1415@gmail.com>
2020-08-15 17:35:34 -0600

    completes keysAndValues

---
+++ keysAndValues.js
@@ -0,0 +1,19 @@
+// Create a function that takes an object and returns the keys and values as separate arrays.
+
+// Examples:
+// keysAndValues({ a: 1, b: 2, c: 3 })
+// ➞ [["a", "b", "c"], [1, 2, 3]]
+// keysAndValues({ a: "Apple", b: "Microsoft", c: "Google" })
+// ➞ [["a", "b", "c"], ["Apple", "Microsoft", "Google"]]
+// keysAndValues({ key1: true, key2: false, key3: undefined })
+// ➞ [["key1", "key2", "key3"], [true, false, undefined]]
+
+// Notes:
+// Remember to sort the keys.
+
+function keysAndValues(obj) {
+  const keys = Object.keys(obj);
+  const values = keys.map((k) => obj[k]);
+  return [keys, values];
+}
+console.log(keysAndValues({ a: 1, b: 2, c: 3 }));
