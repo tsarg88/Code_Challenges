b90ae77a8653ced801a0918699186dabca79df64
umbur <work1415@gmail.com>
2020-08-24 15:10:34 -0600

    completes cardHide

---
+++ cardHide.js
@@ -0,0 +1,21 @@
+// Write a function that takes a credit card number and only displays the last four characters.
+// The rest of the card number must be replaced by ************.
+
+// Examples:
+// cardHide("1234123456785678") ➞ "************5678"
+// cardHide("8754456321113213") ➞ "************3213"
+// cardHide("35123413355523") ➞ "**********5523"
+
+// Notes:
+// Ensure you return a string.
+// The length of the string must remain the same as the input.
+
+function cardHide(card) {
+  const fullNumber = card;
+  const last4Digits = fullNumber.slice(-4);
+  return last4Digits.padStart(fullNumber.length, "*");
+}
+console.log(cardHide("1234123456785678"));
+
+// Solution 2:
+// const cardHide = card => '*'.repeat(card.length - 4) + card.slice(-4);
