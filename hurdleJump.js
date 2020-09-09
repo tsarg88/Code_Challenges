9b6f45ed7885cfe8699d6cda2dc1693eee0e8d3c
umbur <work1415@gmail.com>
2020-09-09 01:52:30 -0600

    completes hurdleJump

---
+++ hurdleJump.js
@@ -0,0 +1,15 @@
+// Create a function that takes an array of hurdle heights and a jumper's jump height, and determine whether or not
+// the hurdler can clear all the hurdles.
+// A hurdler can clear a hurdle if their jump height is greater than or equal to the hurdle height.
+
+// Examples:
+// hurdleJump([1, 2, 3, 4, 5], 5) ➞ true
+// hurdleJump([5, 5, 3, 4, 5], 3) ➞ false
+// hurdleJump([5, 4, 5, 6], 10) ➞ true
+// hurdleJump([1, 2, 1], 1) ➞ false
+
+function hurdleJump(hurdles, jumpHeight) {
+  const main = (x) => jumpHeight >= x;
+  return hurdles.every(main) || hurdles.length === 0 ? true : false;
+}
+console.log(hurdleJump([1, 2, 3, 4, 5], 5));
