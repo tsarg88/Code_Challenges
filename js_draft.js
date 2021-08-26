937f8aceba8d8244848318b11969174f05054818
umbur <work1415@gmail.com>
2021-08-25 18:50:36 -0600

    completes isListPalindrome

--- js_draft.js
+++ js_draft.js
@@ -1250,7 +1250,7 @@
 //   return dummy.next;
 // };
 
-// the full solution
+// the full solution, which includes how to convert arr to list to pass it to ListNode and then covert it back to array
 // function ListNode(x) {
 //   this.value = x;
 //   this.next = null;
@@ -1279,7 +1279,7 @@
 //   return node.next;
 // }
 
-// // All changes are in the part below:
+// All changes are in the part below:
 
 // function arrayToList(arr) {
 //   return arr.reduceRight(
@@ -1323,3 +1323,37 @@
 //   return subArr;
 // }
 // console.log(prefixSums([1, 1, 1, 1], 2));
+
+// the relatively easy solution
+// function isListPalindrome(l) {
+//   l = JSON.stringify(l).slice(1, -1).split(",");
+//   return l + "" == l.reverse();
+// }
+// console.log(isListPalindrome([0, 1, 9]));
+
+// the hard solution using more "pure" LinkedList approach
+function isListPalindrome(list) {
+  var slow = null,
+    fast = list,
+    temp;
+  // Find center point and reverse the first half of the list
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    temp = list.next;
+    list.next = slow;
+    slow = list;
+    list = temp;
+  }
+  // If fast not null, list length is odd, ignore the center value
+  if (fast) {
+    list = list.next;
+  }
+  // Find the first difference
+  while (list) {
+    if (slow.value != list.value) return false;
+    slow = slow.next;
+    list = list.next;
+  }
+  // Return true, if no diff
+  return true;
+}
